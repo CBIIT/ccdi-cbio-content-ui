9ddbedc9af7df99f673b948732f9cda77609a48c
johnyesit <ziao.ye@nih.gov>
2025-06-26 18:03:53 -0400

    Add underline to the link

--- src/app/release-notes/handleReleaseNotes.ts
+++ src/app/release-notes/handleReleaseNotes.ts
@@ -21,7 +21,6 @@ function rehypeCustomTheme() {
         if (node.properties.href === 'dataset-updates') {
           node.properties.id = node.properties.href;
           node.properties.href = 'javascript:void(0);';
-          node.properties.className = ['text-[rgba(69,82,153,1)]'];
         } else {
           node.properties.target = '_blank';
         }
