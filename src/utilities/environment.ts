59cabe508c907e353178ae4e8662f95e936fa342
johnyesit <ziao.ye@nih.gov>
2025-11-14 13:21:24 -0500

    Replace switch with if-else

--- src/utilities/environment.ts
+++ src/utilities/environment.ts
@@ -5,14 +5,14 @@ export const getTierName = (hostname: string): string => {
   const isDevEnv = process.env.NODE_ENV === 'development' || tierNameArr.includes('dev');
   const isQaEnv = tierNameArr.includes('qa');
   const isStageEnv = tierNameArr.includes('stage');
-  switch (true) {
-    case isDevEnv:
-      return 'dev';
-    case isQaEnv:
-      return 'qa';
-    case isStageEnv:
-      return 'stage';
-    default:
-      return 'prod';
+  if (isDevEnv) {
+    return 'dev';
   }
+  if (isQaEnv) {
+    return 'qa';
+  }
+  if (isStageEnv) {
+    return 'stage';
+  }
+  return 'prod';
 };
